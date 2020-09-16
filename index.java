98d2cb6854460c67c648abab064ef9d7529f40ea
ZainabKhan944 <zyk0517@gmail.com>
2020-09-16 19:49:00 +0000

    I drew a cat for my assignment

--- index.java
+++ index.java
@@ -11,14 +11,60 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
-  
+  background(250);
+  stroke(0);
+ rectMode(CORNER);
+ fill(250)
+ rect(235,210, 10, 40);
+ rect(215,215, 10, 40);
+ rect(150,210, 10, 40);
+ rect(180,210, 10, 40);
+ rect(115 ,200, 40, 5);
+ ellipseMode(CENTER); 
+
+
  
-  
-  
+fill(250)
+ellipse(200, 200, 125, 50);
+ellipse(249, 157, 50, 50);
+fill(0)
+ellipse(249, 165, 5, 5);
  
 
-  
- 
-	
+fill(0)
+rect(240,160, 2, 2);
+rect(235,157, 2, 2);
+rect(240,165, 2, 2);
+rect(235,165, 2, 2);
+rect(240,170, 2, 2);
+rect(235,175, 2, 2);
+rect(255,160, 2, 2);
+rect(255,165, 2, 2);
+rect(255,170, 2, 2);  
+rect(260,165, 2, 2);
+rect(260,157, 2, 2);
+rect(260,175, 2, 2);
 
+ // comment
+ fill(0)
+ ellipse(242, 147, 5, 8);
+ ellipse(260, 147, 5, 8);
+
+fill(0)		 
+rect(232,132, 2, 2);
+rect(237,120, 2, 2);
+rect(241,132, 2, 2);
+
+fill(0)
+rect(252,132, 2, 2);
+rect(257,120, 2, 2);
+rect(261,132, 2, 2);
+
+fill(0)
+rect(235,126, 2, 2);
+rect(240,126, 2, 2);
+
+fill(0)
+rect(255,126, 2, 2);
+rect(260,126, 2, 2);
 }
