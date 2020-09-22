7df58da559d5427906d3f0b53c76571567844d15
ZainabKhan944 <zyk0517@gmail.com>
2020-09-22 14:47:53 +0000

    I added some new color

--- index.java
+++ index.java
@@ -14,18 +14,19 @@ void draw() {
   background(250);
   stroke(0);
  rectMode(CORNER);
- // Colors the legs and tail white
- fill(250);
+
  // This coding makes up the legs and tail
- fill(200);
+ fill (200);
  rect(235,210, 10, 40);
  rect(215,215, 10, 40);
  rect(150,210, 10, 40);
  rect(180,210, 10, 40);
+ fill (219, 44, 44);
  rect(115 ,200, 40, 5);
  ellipseMode(CENTER); 
 
-fill(0);
+// This makes up the ears
+fill (219, 44, 44);
 triangle(240,122, 235, 134, 246, 135);
 triangle(260,122, 253, 134, 264, 135);
 // This makes the body, and head color white.
@@ -36,6 +37,7 @@ ellipse(200, 200, 125, 50);
 ellipse(249, 157, 50, 50);
 fill(0);
 // This makes the nose
+fill (227, 159, 219);
 ellipse(249, 165, 5, 5);
 
 // Colors the whiskers black
@@ -49,7 +51,6 @@ line(256, 168, 263, 167);
 line(256, 173, 262, 177);
 
  // This colors the eyes, and ears black
- fill(0);
  // These make the eyes
  ellipse(242, 147, 5, 8);
  ellipse(260, 147, 5, 8);
